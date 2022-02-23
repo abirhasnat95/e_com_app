33047c9333dc637f71c9e32534c52995c75f7064
abirhasnat95 <abirhasnat@2016@gmail.com>
2022-02-23 13:48:13 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:e_com_app/screens/homeScreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return HomeScreen();
+    debugShowCheckedModeBanner:
+    false;
+  }
+}
